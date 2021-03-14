ca6e79891fc03988666d1288f0e44a4328c367e9
John Quinn <jquinn13@dsg3.virtual.crc.nd.edu>
2021-03-14 15:23:46 -0400

    Fully functional

--- sync.py
+++ sync.py
@@ -35,57 +35,57 @@ def syncImages():
     # Query Mews DB
     mewsCursor = mewsCnx.cursor()
     query = ("SELECT url, image_url, reposts, replies, likes, when_posted, "
-    "related_text, ocr_text, when_scraped, original_img_dir, original_img_filename, pic_id FROM scraped_images WHERE when_scraped > %s LIMIT 1")
+    "related_text, ocr_text, when_scraped, original_img_dir, original_img_filename, pic_id FROM scraped_images WHERE when_scraped > %s LIMIT 3")
     mewsCursor.execute(query, (lastSync,))
-
-    # Extract Variables
-    (post_url, image_url, reposts, replies, likes, when_posted, related_text, ocr_text, when_scraped, image_directory, image_filename, scrape_id) = mewsCursor.fetchone()
-    when_updated = when_scraped
-
-    # Disconnect from Mews DB
-    mewsCnx.close()
 
     # Grab Mews-App Config
     with open('config/mews-app.json') as f:
         mewsAppConfig = json.load(f)
 
-    # Connect to Mews-App DB
-    try:
-        mewsAppCnx = mysql.connector.connect(**mewsAppConfig)
-    except mysql.connector.Error as err:
-        print(err)
-        return 1
+    # Extract Variables
+    for result in mewsCursor.fetchall():
+        (post_url, image_url, reposts, replies, likes, when_posted, related_text, ocr_text, when_scraped, image_directory, image_filename, scrape_id) = result
+        when_updated = when_scraped
 
-    # Insert into Mews-App
-    data = {
-            'post_url': post_url,
-            'image_url': image_url,
-            'reposts': reposts,
-            'replies': replies,
-            'likes': likes,
-            'when_posted': when_posted,
-            'related_text': related_text,
-            'ocr_text': ocr_text,
-            'when_scraped': when_scraped,
-            'when_updated': when_updated,
-            'image_directory': image_directory,
-            'image_filename': image_filename,
-            'scrape_id': scrape_id
-            }
-    mewsAppCursor = mewsAppCnx.cursor()
-    query = ("INSERT INTO Posts "
-    "(post_url, image_url, reposts, replies, likes, when_posted, "
-    "related_text, ocr_text, when_scraped, when_updated, image_directory, image_filename, scrape_id) VALUES "
-    "(%(post_url)s, %(image_url)s, %(reposts)s, %(replies)s, %(likes)s, %(when_posted)s, %(related_text)s, "
-    "%(ocr_text)s, %(when_scraped)s, %(when_updated)s, %(image_directory)s, %(image_filename)s, %(scrape_id)s)")
+        # Connect to Mews-App DB
+        try:
+            mewsAppCnx = mysql.connector.connect(**mewsAppConfig)
+        except mysql.connector.Error as err:
+            print(err)
+            return 1
 
-    try:
-        mewsAppCursor.execute(query, data)
-        mewsAppCnx.commit()
-    except mysql.connector.Error as err:
-        print(err)
-        return 1
+        # Insert into Mews-App
+        data = {
+                'post_url': post_url,
+                'image_url': image_url,
+                'reposts': reposts,
+                'replies': replies,
+                'likes': likes,
+                'when_posted': when_posted,
+                'related_text': related_text,
+                'ocr_text': ocr_text,
+                'when_scraped': when_scraped,
+                'when_updated': when_updated,
+                'image_directory': image_directory,
+                'image_filename': image_filename,
+                'scrape_id': scrape_id
+                }
+        mewsAppCursor = mewsAppCnx.cursor()
+        query = ("INSERT INTO Posts "
+        "(post_url, image_url, reposts, replies, likes, when_posted, "
+        "related_text, ocr_text, when_scraped, when_updated, image_directory, image_filename, scrape_id) VALUES "
+        "(%(post_url)s, %(image_url)s, %(reposts)s, %(replies)s, %(likes)s, %(when_posted)s, %(related_text)s, "
+        "%(ocr_text)s, %(when_scraped)s, %(when_updated)s, %(image_directory)s, %(image_filename)s, %(scrape_id)s)")
 
+        try:
+            mewsAppCursor.execute(query, data)
+            mewsAppCnx.commit()
+        except mysql.connector.Error as err:
+            print(err)
+            return 1
+
+    # Disconnect from Mews and Mews-App
+    mewsCnx.close()
     mewsAppCnx.close()
 
 
