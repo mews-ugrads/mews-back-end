bf5e91d0bc1cfbd4924449a2a58105e89990d1f0
John Quinn <johnedquinn@gmail.com>
2021-04-06 15:50:38 -0400

    Initialize mysql setup file

---
+++ config/setup.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `PostRelatedness` (
+  `post1_id` bigint(20) NOT NULL,
+  `post2_id` bigint(20) NOT NULL,
+  `rel_txt_wt` double NOT NULL,
+  `rel_txt_meta` varchar(255) DEFAULT NULL,
+  `sub_img_wt` double NOT NULL,
+  `sub_img_meta` varchar(255) DEFAULT NULL,
+  `ocr_wt` double NOT NULL,
+  `ocr_meta` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`post1_id`,`post2_id`)
+);
