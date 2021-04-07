012607f7dd7835e9214661dea1ca191026a53053
John Quinn <johnedquinn@gmail.com>
2021-04-07 13:49:19 -0400

    Remove constraints

--- config/setup.sql
+++ config/setup.sql
@@ -1,11 +1,11 @@
 CREATE TABLE `PostRelatedness` (
   `post1_id` bigint(20) NOT NULL,
   `post2_id` bigint(20) NOT NULL,
-  `rel_txt_wt` double NOT NULL,
+  `rel_txt_wt` double DEFAULT NULL,
   `rel_txt_meta` varchar(255) DEFAULT NULL,
-  `sub_img_wt` double NOT NULL,
+  `sub_img_wt` double DEFAULT NULL,
   `sub_img_meta` varchar(255) DEFAULT NULL,
-  `ocr_wt` double NOT NULL,
+  `ocr_wt` double DEFAULT NULL,
   `ocr_meta` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`post1_id`,`post2_id`)
 );
